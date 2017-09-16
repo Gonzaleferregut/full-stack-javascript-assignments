48a979ad40824ae06346c4e25bb340b71f8043d8
Rigoberto Gonzaleferregut <rigoberto.gonzaleferregut@gmail.com>
2017-09-16 15:01:59 -0500

    completed condition test assignment

--- src/javascript/intro/grammer/control-flow/conditionals/index.js
+++ src/javascript/intro/grammer/control-flow/conditionals/index.js
@@ -1,8 +1,20 @@
-export const greeting = (age = 0) => {
-
-};
+export const greeting = (age = 0) => age > 18 ? "Hello!" : "Yo!";
 
 export const isTacoTuesday = (day = '') => {
-
+  let foo = day.toLowerCase();
+  switch (foo) {
+    case 'monday':
+    case 'wednesday':
+    case 'thursday':
+    case 'friday':
+    case 'saturday':
+    case 'sunday':
+      return false;
+      break;
+    case 'tuesday':
+      return true;
+      break;
+    default:
+      return day + ' is not taco tuesday!';
+  }
 };
-
